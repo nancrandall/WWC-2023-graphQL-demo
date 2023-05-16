218336c32df7a46fa66d23fd1f33c2940a28ee83
Nancy-Crandall <nancy.crandall@cvshealth.com>
2023-05-16 10:58:35 -0600

    added in graphql server and updated readme docs

---
+++ wwcode-dev-summit-speakers/src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+//Template literal string of the GraphQL Query
+
+function App() {
+  const [speakers, setSpeakers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+        {
+          speakers {
+            id
+            name
+            topics {
+              name
+            }
+          }
+        }
+        `,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data.data);
+        // Set the state
+        setSpeakers(data.data.speakers);
+      })
+      .catch((err) => console.error("ERROR: ", err));
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Thank you Women Who Code Dev Summit 2023 Speakers!</h1>
+        <ul>
+          {/* JavaScript value from the State to a JSON value */}
+          {/* {JSON.stringify(speakers, null, 2)} */}
+          {speakers.map((speakerElement) => (
+            <li key={speakerElement.id}>{speakerElement.name}</li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
